fea26eb360721025fdb442e987726478be47f8f3
swordtk0309 <dreamhigh924@gmail.com>
2023-01-12 14:51:18 +0900

    test

--- src/main/java/com/naver/idealproduction/bookmarket/BookMarketApplication.java
+++ src/main/java/com/naver/idealproduction/bookmarket/BookMarketApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookMarketApplication {
 
 	public static void main(String[] args) {
+		// work branch
 		SpringApplication.run(BookMarketApplication.class, args);
 	}
 
