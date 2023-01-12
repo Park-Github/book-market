633a69e524f3c86edff911e17b5fa7068739a8c6
lazoyoung <idealproduction@naver.com>
2023-01-12 14:37:18 +0900

    Update member-register.js

--- src/main/resources/static/js/member-register.js
+++ src/main/resources/static/js/member-register.js
@@ -3,6 +3,10 @@ let input_pwd;
 let id_feedback;
 let button_submit;
 
+/**
+ * This function will run once every DOM element is loaded
+ * https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduction
+ */
 window.onload = function () {
     input_id = document.getElementById('id');
     input_pwd = document.getElementById('pwd');
@@ -11,36 +15,43 @@ window.onload = function () {
     let button_reset = document.getElementById('form-reset-btn');
     input_id.addEventListener('blur', e => validateId(e));
     input_pwd.addEventListener('blur', e => validatePassword(e));
-    button_reset.addEventListener('click', () => resetValidations());
-    resetValidations();
+    button_reset.addEventListener('click', () => resetValidity());
+    resetValidity();
 }
 
+/**
+ * Checks if input_id has valid and unique ID entry
+ * and change form validity accordingly.
+ */
 function validateId(e) {
-    let target = e.target;
-    let value = target.value;
+    let id_entry = e.target.value; // String
 
-    if (!value) {
+    if (id_entry.length <= 6) {
         changeValidity(input_id, false);
-        id_feedback.innerText = 'This field is empty!';
+        id_feedback.innerText = (!id_entry) ? 'This field is empty!' : 'Too short id!';
         return;
     }
 
-    let query = new URLSearchParams({id: value});
-    fetch('/member/register/validate-id?' + query)
+    // Fetch HTTP GET request. https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    let url = '/member/register/validate-id?';
+    let query = new URLSearchParams({id: id_entry});
+    fetch(url + query)
         .then((r) => {
-            if (!r.ok) {
+            if (!r.ok)
                 throw new Error(`HTTP Error ${r.status}: ${r.statusText}`);
-            }
-            return r.text();
+
+            return r.text(); // Response is fetched into String (result)
         })
-        .then((text) => {
-            changeValidity(input_id, text === 'true');
-        });
+        .then((result) => changeValidity(input_id, result === 'true'));
 }
 
+/**
+ * Checks if input_pwd has valid PASSWORD entry
+ * and change form validity accordingly.
+ */
 function validatePassword(e) {
     let value = e.target.value;
-    let valid = value.length >= 6;
+    let valid = (value.length >= 6);
     changeValidity(input_pwd, valid);
 }
 
@@ -53,7 +64,7 @@ function changeValidity(input, valid) {
     updateSubmitButton();
 }
 
-function resetValidations() {
+function resetValidity() {
     input_id.classList.remove('is-valid', 'is-invalid');
     input_pwd.classList.remove('is-valid', 'is-invalid');
     updateSubmitButton();
