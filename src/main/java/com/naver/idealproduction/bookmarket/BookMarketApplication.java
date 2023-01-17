4baee6ab89e8813f18e3b41c962e7fdf055ec485
lazoyoung <idealproduction@naver.com>
2023-01-18 01:51:25 +0900

    Enforce cookies to accept http requests only

--- src/main/java/com/naver/idealproduction/bookmarket/BookMarketApplication.java
+++ src/main/java/com/naver/idealproduction/bookmarket/BookMarketApplication.java
@@ -1,23 +1,27 @@
 package com.naver.idealproduction.bookmarket;
 
 import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.SessionTrackingMode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.WebApplicationInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.EnumSet;
 
 @SpringBootApplication
-public class BookMarketApplication implements WebApplicationInitializer {
+public class BookMarketApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookMarketApplication.class, args);
 	}
 
 	@Override
-	public void onStartup(ServletContext servletContext) {
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
 		servletContext.setSessionTimeout(60);
 		servletContext.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
+		var sessionCookieConfig = servletContext.getSessionCookieConfig();
+		sessionCookieConfig.setHttpOnly(true);
 	}
 }
