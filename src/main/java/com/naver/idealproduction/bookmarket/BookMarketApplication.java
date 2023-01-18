2a00c3c2494af931a06fa3359e79e56b7d70a3e7
lazoyoung <idealproduction@naver.com>
2023-01-18 23:35:06 +0900

    Fix ServletContext not properly handled on startup.

--- src/main/java/com/naver/idealproduction/bookmarket/BookMarketApplication.java
+++ src/main/java/com/naver/idealproduction/bookmarket/BookMarketApplication.java
@@ -1,24 +1,22 @@
 package com.naver.idealproduction.bookmarket;
 
 import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.SessionTrackingMode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 
 import java.util.EnumSet;
 
 @SpringBootApplication
-public class BookMarketApplication extends SpringBootServletInitializer {
+public class BookMarketApplication implements ServletContextInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookMarketApplication.class, args);
 	}
 
 	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		super.onStartup(servletContext);
+	public void onStartup(ServletContext servletContext) {
 		servletContext.setSessionTimeout(60);
 		servletContext.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
 		var sessionCookieConfig = servletContext.getSessionCookieConfig();
