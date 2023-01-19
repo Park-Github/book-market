27f6df0a1017f0cb319d7604e97aa36d96b7ce19
lazoyoung <idealproduction@naver.com>
2023-01-19 11:22:55 +0900

    Alter nav-link attributes to match current page

--- src/main/resources/static/js/template.js
+++ src/main/resources/static/js/template.js
@@ -1,11 +1,28 @@
+class PageName extends HTMLElement {
+    constructor() {
+        super();
+
+        const id = 'link-' + this.innerText.toLowerCase();
+        let a = document.getElementById(id);
+        this.innerText = null;
+
+        if (a) {
+            a.classList.add('active');
+            a.setAttribute('aria-current', 'page');
+        }
+    }
+}
+
 window.addEventListener('load', () => {
     let logout_form = document.getElementById('logout-form');
     let logout_button = document.getElementById('logout-btn');
 
     if (logout_button) {
+
         logout_button.addEventListener('click', (e) => {
             e.preventDefault();
             logout_form.submit();
         });
     }
+    customElements.define('page-name', PageName);
 });
